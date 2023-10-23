1f9140c104c155ff8a5c73d28d49479028afa23a
nicolasgomez1936 <nicolasgomez1936@gmail.com>
2023-10-23 19:09:54 -0300

    segunda reactjs

---
+++ src/components/item/index.jsx
@@ -0,0 +1,44 @@
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import "./index.css"
+import ItemCount from '../item-count';
+
+const Item = ({ title, price, stock, img, items }) => {
+
+  const onAddProduct = () => {
+    console.log("Cantidad de productos agregados al carrito", items)
+  }
+
+  return (
+    <div className="cardContainer">
+      <Card sx={{ maxWidth: 345, backgroundColor: "rgba(0, 0, 0, 0.26)" }}>
+        <CardMedia
+          sx={{ height: 140 }}
+          image={img}
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {title}
+          </Typography>
+          <Typography variant="body2" style={{ color: "black", fontFamily: "revert", fontSize: "medium" }}>
+            Precio: ${price}
+          </Typography>
+          <Typography variant="body2" style={{ color: "black", fontFamily: "revert", fontSize: "medium" }}>
+            Stock: {stock}
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <ItemCount initial={1} stock={10}/>
+          <Button style={{ color: "black", fontWeight: "bold", fontFamily: "unset" }} size="small" onClick={() => onAddProduct()} disabled={!stock}>Add to cart</Button>
+        </CardActions>
+      </Card>
+    </div>
+  );
+}
+
+export default Item
